4f69adaeb57a7411ddf8d0a565c60e23d2e20b2d
KyotoTommy <yasutomi.takahiro77@gmail.com>
2016-04-22 07:53:15 +0900

    Update RBMFunc.cc

--- RBMFunc.cc
+++ RBMFunc.cc
@@ -1,28 +1,17 @@
 #include "RBMFunc.h"
 
-void RBM::setLayer(int layerNumtmp, int *nodeNumtmp){
-  layerNum = layerNumtmp;
-  
-  for(int i=0; i<nodeNumtmp.size(); i++)
-    nodeNum.push_back(nodeNumtmp[i]);
-
-  h.resize(layerNum);
-
-}
-
 void RBM::Efunc(){
 
-  for(int r=0; r<layerNum; r++){
     for(int i=0; i<nodeNum[i]; i++){
       for(int j=0; j<nodeNum[j]; j++){
 
-	Efunc = -w[i][j]*h[r-1][i]*h[r][j];
+	Efunc += -b[i]*v[i] - c[j]*h[j] -w[i][j]*v[i]*c[j];
 
       }}}
 
 }
 
-void RBM::preLearning(int layer){
+void RBM::Learning(){
 
 
 }
