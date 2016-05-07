a1b8e39e37cdb5ab5cd23fe5a6841a8b3177be35
KyotoTommy <yasutomi.takahiro77@gmail.com>
2016-05-07 10:11:48 +0900

    finish update parameter code

--- RBMFunc.cc
+++ RBMFunc.cc
@@ -12,7 +12,21 @@ void RBM::Efunc(){
 }
 
 void RBM::updatePara(){
+int i,j;
 
+//update  W
+for(i=0; i<visibleNum; i++){
+	for(j=0; j<hiddenNum; j++)
+		w[i][j] += alpha * ( visible[i]*prob_h_v(j,visible[i]) - visible_1[i]*prob_h_v(j,visible_1[i]) );
+}
+
+//update b
+for(i=0; i<visibleNum; i++)
+	b[i]    += alpha * ( visible[i] - visible_1[i] );
+
+//update h
+for(j=0; j<hiddenNum; j++)
+	c[j]    += alpha * ( prob_h_v(j,visible[i]) - prob_h_v(j,visible_1[i]) );
 
 }
 
@@ -20,3 +34,9 @@ void RBM::CDmethod(){
 
 
 }
+
+double RBM::sigmoid(double tmp){
+return 1 / ( 1 + exp( -tmp ) );
+}
+
+double RBM::lambda()
