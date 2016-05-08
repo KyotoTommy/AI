991fc817e5fd17891f04a2a8fdeca6ac1ada87b4
KyotoTommy <yasutomi.takahiro77@gmail.com>
2016-05-08 09:37:04 +0900

    add constractor

--- RBMFunc.h
+++ RBMFunc.h
@@ -7,12 +7,21 @@ class RBM{
   double Efunc;
   vector< vector<double> > w;
   vector<double> b,c,v,h;
+  int visibleNum, hiddenNum;
 
   public:
   void Efunc();
   void updatePara();
   void CDmethod();
-
+　void RBM(int i, int j){
+　 visibleNum = i;
+　 b.resize(i);
+　 hiddenNum = j;
+　 c.resize(j)
+　 w.resize(i);
+　 for(int k=0; k<i; k++)
+　  w[i].resize(j);
+　}
 };
 
 #endif// _RBMFUNC_H_
