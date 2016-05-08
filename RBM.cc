4a4854eba125f6c6cabbfea855370bd3e663e0e9
KyotoTommy <yasutomi.takahiro77@gmail.com>
2016-05-08 09:51:36 +0900

    Update RBM.cc

--- RBM.cc
+++ RBM.cc
@@ -2,10 +2,17 @@
 
 int main(int argc, char *argv[])
 {
+    vector<int> v0{1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<int> v1{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<int> v2{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0};
+    vector<int> v3{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1};
+    
+  RBM RBM(20,10);
   
-  RBM RBM(10,20);
+  updatePara(V0,0.01);
+  updatePara(V1,0.01);
+  updatePara(V2,0.01);
+  updatePara(V3,0.01);
   
-  
-
   return 0;
 }
